565ca2572623bff9ef00282ed9198c5ee6c37c42
Jack Youstra <jack@youstra.com>
2015-03-22 20:17:26 -0700

    Initial commit

---
+++ lua/entities/shared.lua
@@ -0,0 +1,11 @@
+ENT.Type = "anim"
+
+ENT.PrintNmae = "CIWS System"
+ENT.Author = "Jack Youstra"
+ENT.Information = "A gun turret that shoots things down"
+ENT.Category = "Bastion Anti-Air"
+
+ENT.Editable = true
+ENT.Spawnable = true
+ENT.AdminOnly = false
+ENT.RenderGroup = RENDERGROUP_OPAQUE
